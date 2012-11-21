470cb7a6395a18df4c821fbcd3def015838e5ffa
Sriram Karra <karra.etc@gmail.com>
2012-11-21 21:54:22 +0530

    Refactor the main handler to make it more modular object oriented

--- Mailman/Cgi/ctl.py
+++ Mailman/Cgi/ctl.py
@@ -30,7 +30,6 @@ from Mailman.tornado import template
 _ = i18n._
 i18n.set_language(mm_cfg.DEFAULT_SERVER_LANGUAGE)
 
-
 def auto_version (resource):
     """Intended to be invoked from inside tornado templates, given a resource
     names such as /static/ctl.js this will return something like
@@ -48,88 +47,116 @@ def auto_version (resource):
     res = re.match('(.*\.)([a-zA-Z]*$)', resource)
     return ''.join([res.group(1), timestamp, '.', res.group(2)])
 
-
-def ctl_home ():
-    loader = template.Loader("../templates/en")
-    print 'Content-Type: text/html; charset=%s\n' % 'us-ascii'
-    print
-    print loader.load("ctl-base.html").generate(auto_version=auto_version,
-                                                curr_user=curr_user())
 
-def ctl_view ():
-    loader = template.Loader("../templates/en")
-    print 'Content-Type: text/html; charset=%s\n' % 'us-ascii'
-    print
+def get_curr_user ():
+    """Extracts the Cleartrip userid from the cookie. This cookie is set by
+    any authenticated Cleartrip site - and as a result the site where this
+    list server will be hosted."""
 
-    listnames = Utils.list_names()
-    listnames.sort()
-
-    lists = []
-    for name in listnames:
-        mlist   = MailList.MailList(name, lock=0)
-        members = mlist.getRegularMemberKeys()
-        subscribed = True if curr_user() in members else False
-
-        lists.append({'script_url'  : mlist.GetScriptURL('listinfo'),
-                      'real_name'   : mlist.real_name,
-                      'description' : Utils.websafe(mlist.description),
-                      'subscribed'  : subscribed,
-                      'owners'      : ', '.join(mlist.owner),
-                      'owner-email' : mlist.GetOwnerEmail(),
-                      })
-
-    print loader.load("ctl-view.html").generate(auto_version=auto_version,
-                                                curr_user=curr_user(),
-                                                lists=lists)
-
-def ctl_create ():
-    loader = template.Loader("../templates/en")
-    print 'Content-Type: text/html; charset=%s\n' % 'us-ascii'
-    print
-    print loader.load("ctl-create.html").generate(auto_version=auto_version,
-                                                  curr_user=curr_user())
-
-def ctl_admin ():
-    loader = template.Loader("../templates/en")
-    print 'Content-Type: text/html; charset=%s\n' % 'us-ascii'
-    print
-    print loader.load("ctl-admin.html").generate(auto_version=auto_version,
-                                                 curr_user=curr_user())
-
-_curr_user = None
-
-def curr_user (init=False):
-    global _curr_user
-
-    if not init and _curr_user:
-        return _curr_user
-
+    _curr_user = None
     try:
         cookie = Cookie.SimpleCookie(os.environ["HTTP_COOKIE"])
         _curr_user = string.replace(cookie["userid"].value, '%40', '@')
     except Cookie.CookieError, e:
         _curr_user = "Take a hike, Mike - C"
     except  KeyError, e:
-        _curr_user = "Take a hike, Mike - K"    
+        _curr_user = "Take a hike, Mike - K"
+
+    return _curr_user
+
+
+class Action:
+    def __init__ (self, templ):
+        self._templ     = templ
+        self._curr_user = get_curr_user()
+        self._templ_dir = "../templates/en"
+        self.kwargs     = {'auto_version' : auto_version,
+                           'curr_user'    : self.curr_user,
+                           }
+
+    @property
+    def templ_dir (self): 
+        return self._templ_dir;
+
+    @property
+    def templ (self): 
+        return self._templ;
+
+    @property
+    def curr_user (self):
+        return self._curr_user
+
+    def kwargs_add (self, key, val):
+        self.kwargs.update({key : val})
+
+    def render (self):
+        loader = template.Loader(self.templ_dir)
+        print 'Content-Type: text/html; charset=%s\n' % 'us-ascii'
+        print
+        print loader.load(self.templ).generate(**self.kwargs)
+
+    def handler (self):
+        """To be overridden, but we have a simple default, regardless"""
+        self.render()
+
+
+class Home(Action):
+    def __init__ (self):
+        Action.__init__(self, "ctl-base.html")
+
+
+class View(Action):
+    def __init__ (self):
+        Action.__init__(self, "ctl-view.html")
+    
+    def handler (self):
+        listnames = Utils.list_names()
+        listnames.sort()
+    
+        lists = []
+        for name in listnames:
+            mlist   = MailList.MailList(name, lock=0)
+            members = mlist.getRegularMemberKeys()
+            subscribed = True if self.curr_user in members else False
+    
+            lists.append({'script_url'  : mlist.GetScriptURL('listinfo'),
+                          'real_name'   : mlist.real_name,
+                          'description' : Utils.websafe(mlist.description),
+                          'subscribed'  : subscribed,
+                          'owners'      : ', '.join(mlist.owner),
+                          'owner-email' : mlist.GetOwnerEmail(),
+                          })
+    
+        self.kwargs_add('lists', lists)
+        self.render()
+
+
+class Create(Action):
+    def __init__ (self):
+        Action.__init__(self, "ctl-create.html")
+
+
+class Admin(Action):
+    def __init__ (self):
+        Action.__init__(self, "ctl-admin.html")
+
 
 def main ():
     parts = Utils.GetPathPieces()
     if not parts:
-        ctl_home()
+        Home().handler()
         return
-
-    curr_user(init=True)
 
     action = parts[0].lower()
     if action == 'view':
-        ctl_view()
+        View().handler()
     elif action == 'create':
-        ctl_create()
+        Create().handler()
     elif action == 'admin':
-        ctl_admin()
+        Admin().handler()
     else:
         ## FIXME: This should throw a 404.
-        ctl_home()
+        Home().handler()
 
 
 if __name__ == "__main__":
